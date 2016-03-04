d270e12aa961b0b3fe552575eb11913b64e9cc77
unixbash <filip.nikolic2@mail.dcu.ie.com>
2016-03-04 15:41:45 +0000

    Addition of an example that uses a PIR sensor, turning on an LED when motion is detected.

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,6 +9,7 @@ print("Nice to meet you " + username)
 """
 
 #E2
+"""
 pin = 7
 delay = 0.5
 
@@ -23,6 +24,32 @@ try:
         time.sleep(delay)
         delay = random.randint(1,90) / 100
         #print(delay)
+
+except KeyboardInterrupt:
+    print("\nKeyboard interupt.")
+except:
+    print("\nAn error occurred!")
+finally:
+    GPIO.cleanup()
+"""
+
+#E3
+led = 7
+pir = 11
+delay = 0.1
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(pir, GPIO.IN)
+
+try:
+    while True:
+
+        if(GPIO.input(pir) == 1):
+            GPIO.output(led, 1)
+            
+        time.sleep(delay)
+        GPIO.output(led, 0)
 
 except KeyboardInterrupt:
     print("\nKeyboard interupt.")
