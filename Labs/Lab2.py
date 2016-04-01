0f08f011a7195daca00400f19e96cf2d21c22816
unixbash <filip.nikolic2@mail.dcu.ie.com>
2016-04-01 09:54:41 +0000

    A backup of Lab1 & Lab2.

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -10,11 +10,36 @@ import codebug_i2c_tether
 
 codebug=codebug_i2c_tether.CodeBug()
 codebug.open()
+codebug.clear()
 
-#Q1 Create a variable called favNum and make it equal to your
-#favourite digit, afterwards use codebug.write_text(0,0,str(favNum))
+#Q1 Change the two boolean values to that you will see all numbers printed
+"""
+a=False
+b=True
 
-favNum=5
-codebug.write_text(0,0,str(favNum))
+#Given Code:
+if(a and b):
+    codebug.write_text(0,0,"1")
+elif(a):
+    codebug.write_text(0,0,"2")
+elif(not a and not b):
+    codebug.write_text(0,0,"3")
+else:
+    codebug.write_text(0,0,"4")
+"""
 
-#Q2
+#Q2 Ask the user for their name and use "" to say "Hello name!" 
+"""
+name=input("What is your name?\n")
+name="Hello " + name + "!"
+
+#Code given
+i=0
+while(i > -5*(len(name))):
+    codebug.write_text(i,0,name)
+    i-=1
+    time.sleep(0.25)
+"""
+
+#Q3 
+
