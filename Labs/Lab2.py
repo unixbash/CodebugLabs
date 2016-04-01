47371fe6a8b1926cd9ce73691743030f645cda21
unixbash <filip.nikolic2@mail.dcu.ie.com>
2016-04-01 14:22:14 +0000

    Completed Lab3.

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -4,13 +4,44 @@
 
     In this lab students will learn flow controll, mainly if statements
 """
-
+import random
 import time
 import codebug_i2c_tether
 
 codebug=codebug_i2c_tether.CodeBug()
 codebug.open()
 codebug.clear()
+
+#Q4 function
+def binaryToDecimal():
+    rand=random.randint(1,31)
+        
+    codebug.set_row(2,rand)
+    answer=input("What number is this in decimal notaton?\n")
+    if(int(answer) == rand):
+        print("You are correct!")
+    else:
+        print(" You got it wrong this time, the answer is " + str(rand))
+
+#Q5 function
+def logicGates(operatorAnd):
+    num1=random.randint(1,31)
+    num2=random.randint(1,31)
+    result=num1 | num2
+    
+    if(operatorAnd):
+        result=num1 & num2
+
+    codebug.set_row(2,num1)
+    codebug.set_row(1,num2)
+    
+    answer=input("What result in decimal notaton?\n")
+    if(int(answer) == result):
+        print("You are correct!")
+    else:
+        print(" You got it wrong this time, the answer is " + str(rand))
+        
+    codebug.set_row(4,result)
 
 #Q1 Change the two boolean values to that you will see all numbers printed
 """
@@ -41,5 +72,29 @@ while(i > -5*(len(name))):
     time.sleep(0.25)
 """
 
-#Q3 
+#Q3 Make a "name length count program", which will ask the user for a sentence
+#and count the length of their name(0-9)
+"""
+name=input("Please enter a sentence:\n")
+
+codebug.write_text(0,0,str(len(name)))
+"""
+
+
+#4 Test your binary number knowledge using the CodeBug, there will be a 5bit number
+#displayed on the grid and the program will ask you what the decimal equivalent is
+"""
+binaryToDecimal()
+"""
+
+#5 Continuing with the binary theme, we will cover binary logic, ie. AND & OR
+
+#a)The AND Gate
+"""
+logicGates(True)
+"""
+
+#b)The OR Gate
+
+logicGates(False)
 
--- Labs/Lab3.py
+++ Labs/Lab3.py
@@ -4,33 +4,57 @@
 
     In this lab students will learn loops, while and for
 """
+import random
 import time
 import codebug_i2c_tether
 
 codebug=codebug_i2c_tether.CodeBug()
 codebug.open()
+codebug.clear()
 
-#Q1 Create a variable called favNum and make it equal to your
-#favourite digit, afterwards use codebug.write_text(0,0,str(favNum))
+#Q1 function
+def randNumber():
+    rand=random.randint(1,9)
+    codebug.write_text(0,0,str(rand))
+    return rand
 
-favNum=5
-codebug.write_text(0,0,str(favNum))
-
-#Q2
-
-
+#Q1 Write a program that will check whether the number on the CodeBug is
+#odd or even, if odd print "Odd!" or if even, print "Even!"
+#Hint: use the % operator
 """
-import codebug_i2c_tether
-import time
+randomNumber=randNumber()#Given
 
-codebug=codebug_i2c_tether.CodeBug()
-codebug.open()
-
-#Q1 Create a program that will count from 0 to 9 on the CodeBug
-num=0
-while(num < 10):
-    codebug.clear()
-    codebug.write_text(0,0,str(num))
-    num += 1
-    time.sleep(1)
+if(randomNumber % 2 == 0):
+    print("Even!")
+else:
+    print("Odd!")
 """
+    
+#Q2 Write a program that will check which button is being pushed by the user.
+#Make sure you sleep for two seconds, before you check and use indicate buttons
+#pressed using arrows on the LED grid, ie. if A is pressed have a left pointing
+#arrow, if both are pressed, point the arrow upwards.
+#HINT: use codebug.get_input('A') to check which button was pressed
+
+time.sleep(2)
+if(codebug.get_input('A') and codebug.get_input('B')):
+    codebug.set_row(4, 0b00100)
+    codebug.set_row(3, 0b01110)
+    codebug.set_row(2, 0b00100)
+    codebug.set_row(1, 0b00100)
+    codebug.set_row(0, 0b00100)
+elif(codebug.get_input('A')):
+    codebug.set_row(4, 0b11100)
+    codebug.set_row(3, 0b11000)
+    codebug.set_row(2, 0b10100)
+    codebug.set_row(1, 0b00010)
+    codebug.set_row(0, 0b00001)
+elif(codebug.get_input('B')):
+    codebug.set_row(4, 0b00111)
+    codebug.set_row(3, 0b00011)
+    codebug.set_row(2, 0b00101)
+    codebug.set_row(1, 0b01000)
+    codebug.set_row(0, 0b10000)
+
+
+
