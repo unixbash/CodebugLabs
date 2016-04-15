85e8b3cb9d982fe5451806a6a3fab31946c7c79e
unixbash <filip.nikolic2@mail.dcu.ie.com>
2016-04-15 16:04:22 +0000

    Completed Lab 8 questions.

--- Labs/Lab8.py
+++ Labs/Lab8.py
@@ -2,8 +2,9 @@
     ***In all labs, students are expected to use Scratch code creator/emulator
     on CodeBug's website in order to understand the logic of the program, before they run it.
 
-    In this lab students will learn about searching throught lists and there will be
-    a bonus question at the end of the lab testing their programming skills.
+    In this lab students will learn about linearly searching throught a list and second
+    question at the end of the lab will test the students programming skills by combining
+    sorting, functions, lists, etc.
 """
 import random
 import time
@@ -19,7 +20,7 @@ codebug.clear()
 #tell the user, value not found by drawing an X on the CodeBug
 #Hint: to make your code cleaner, write two functions one displaying X and
 #the other creating a list of 10 randomly generated number 0-50
-
+"""
 def drawX():
     codebug.set_row(4,0b10001)
     codebug.set_row(3,0b01010)
@@ -32,15 +33,42 @@ def generateList():
         num.append(random.randint(0,50))
     return num
 
-guess=input("Guess a number from 0 to 50:\n")
 num=generateList()
-n=0
 print(num)
+
+guess=input("Guess a number from 0 to 50:\n")
+n=0
 while(n < len(num)):
     if(num[n]==int(guess)):
         codebug.write_text(0,0,str(n))
+        break
     n+=1
     if(n == len(num)):
         drawX()
-    
+
+"""
   
+#Q2 Write a function that will visualy represent a bubble sort algorithm using the
+#CodeBug's LEDs. The list will have 5 elements and they will be represented
+#vertically, while their poition in the array will be represented horizontally
+#HInt: Use CodeBug's set_col(0,0b10001) function.
+"""
+def printAll(num):
+    codebug.clear()
+    for i in range(0,5):
+        codebug.set_col(i,num[i])
+
+
+num=[0b11100,0b11110,0b10000,0b11111,0b11000]
+length=len(num)-1
+sorted=False
+
+while not sorted:
+    sorted=True
+    for i in range(length):
+        if num[i]> num[i+1]:
+            sorted=False
+            num[i], num[i+1] = num[i+1], num[i]    
+        printAll(num)
+        time.sleep(1)
+"""
