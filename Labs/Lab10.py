c647ce805f6824f54fc7dd029c03d38fc1d82cd1
unixbash <filip.nikolic2@mail.dcu.ie.com>
2016-05-23 19:49:33 +0000

    Lab 9 & 10 completed.

--- Labs/Lab10.py
+++ Labs/Lab10.py
@@ -2,6 +2,134 @@
     ***In all labs, students are expected to use Scratch code creator/emulator
     on CodeBug's website in order to understand the logic of the program, before they run it.
 
-    In this lab students will learn how to use the pi & CodeBug with sensors
-    and a camera in order to make a motion triggered camera.
+    In this lab students will learn about Classes and put all test their programming skills.
+"""
+import os
+import sys
+import picamera
+import random
+import time
+import codebug_i2c_tether
+import RPi.GPIO as GPIO
+
+#Set up the GPIO Pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.IN)
+
+codebug=codebug_i2c_tether.CodeBug()
+codebug.open()
+codebug.clear()
+
+#Q1 Create your own Codebug class called Bug. Have three variables called name,
+#age and face. Create a function called getName (print out the name of the Bug),
+#getAge(print age on LED arrray) and getFace (Which will show the face of the codebug
+#on the LED grid). Make sure you have a constructor which will initialize the
+#name, age and face of the Bug
+
+class Bug:
+    name=""
+    age=0
+
+    def __init__(self,name,age,face):
+        self.name=name
+        self.age=age
+        self.face=face
+
+    def getName(self):
+        for letter in self.name:
+            codebug.clear()
+            codebug.write_text(0,0,letter)
+            time.sleep(.5)
+            
+    def getAge(self):
+        for letter in str(self.age):
+            codebug.clear()
+            codebug.write_text(0,0,letter)
+            time.sleep(.5)
+        
+    def getFace(self):
+        if(self.face=="happy"):
+            codebug.set_row(4, 0b01010)
+            codebug.set_row(3, 0b01010)
+            codebug.set_row(2, 0b00000)
+            codebug.set_row(1, 0b10001)
+            codebug.set_row(0, 0b01110)
+        elif(self.face=="sad"):
+            codebug.set_row(4, 0b01010)
+            codebug.set_row(3, 0b01010)
+            codebug.set_row(2, 0b00000)
+            codebug.set_row(1, 0b01110)
+            codebug.set_row(0, 0b10001)
+        else:
+            codebug.set_row(4, 0b01010)
+            codebug.set_row(3, 0b01010)
+            codebug.set_row(2, 0b00000)
+            codebug.set_row(1, 0b11111)
+            codebug.set_row(0, 0b00000)
+
+bug = Bug("Bob",21,"Happy")
+bug.getName()
+time.sleep(2)
+codebug.clear()
+
+bug.getAge()
+time.sleep(2)
+codebug.clear()
+
+bug.getFace()
+time.sleep(2)
+codebug.clear()
+
+#Q2 ***HARD
+#Create a class named SensorCam, inside have a variable called filanme
+#and three functions, arrowLeft (printing out an arrow pointing top left),
+#smile (printing a smiley face) and takePicture, which will take a picture
+#using the PiCam, whenever the PIR sensor is set off. *Make usre you have an
+#arming system, ie user will press a button when ready to activate the system. 
+
+"""
+class SensorCam:
+    filename=""
+    def __init__(self,filename):
+        self.filename=filename
+    
+    def arrowLeft(self):
+        codebug.set_row(4, 0b11100)
+        codebug.set_row(3, 0b11000)
+        codebug.set_row(2, 0b10100)
+        codebug.set_row(1, 0b00010)
+        codebug.set_row(0, 0b00001)
+        time.sleep(1)
+        codebug.clear()
+
+    def smile(self):
+        codebug.set_row(4, 0b01010)
+        codebug.set_row(3, 0b01010)
+        codebug.set_row(2, 0b00000)
+        codebug.set_row(1, 0b10001)
+        codebug.set_row(0, 0b01110)    
+
+    #Take picture using the camera
+    def takePicture(self):
+        with picamera.PiCamera() as camera:
+            camera.resolution=(1600,1200)
+            #camera.start_preview() #Start camera preview, be careful though might crash the Pi
+            time.sleep(1)
+            while(GPIO.input(8) != 1 or codebug.get_input("A")==1):
+                time.sleep(.1)
+            camera.capture(self.filename)          
+
+filename=input("What do you want the file to be called?\n")
+filename=filename+".jpg" 
+cam = SensorCam(filename) 
+
+while(True):
+    codebug.clear()
+    cam.arrowLeft()
+    if(codebug.get_input("A")==1):
+        cam.smile()
+        time.sleep(.2)
+        cam.takePicture()
+
+    time.sleep(.2)
 """
--- Labs/Lab9.py
+++ Labs/Lab9.py
@@ -2,5 +2,65 @@
     ***In all labs, students are expected to use Scratch code creator/emulator
     on CodeBug's website in order to understand the logic of the program, before they run it.
 
-    In this lab students will combine what they have learned so far and make a gane on the Codebug
+    In this lab students will learn about Python File I/O
+"""
+import os
+import sys
+import picamera
+import random
+import time
+import codebug_i2c_tether
+
+codebug=codebug_i2c_tether.CodeBug()
+codebug.open()
+codebug.clear()
+
+#Q1 Read in a text file and there will be a single integer,
+#represent that integer on the CodeBug LED display.
+
+"""
+file = open("num.txt","r+")
+num = file.read(1)
+
+codebug.write_text(0,0,str(num))
+file.close
+"""
+
+#Q2 Create a number counter, that will flash an LED every time you press a
+#button on the CodeBug and increment a counter, when the user presses the other
+#button all LEDs should flash and the result of the count should be saved in a
+#text file called counted.txt
+
+"""
+counter=0
+
+while(codebug.get_input('A') != 1):
+    codebug.clear()
+    if(codebug.get_input('B')==1):
+        counter+=1
+        codebug.set_pixel(2,2,1)
+        time.sleep(0.2)
+    time.sleep(0.1)
+    
+codebug.set_row(2,0b11111)
+time.sleep(0.2)
+codebug.clear()
+
+with open("counted.txt","w") as resultF:
+    print(format(counter), file=resultF)
+"""
+
+#Q3 Create a program that will take a picture using the PiCamea when a button on
+#the Codebug is pressed. Make the image file name the current date and time.
+
+"""
+with picamera.PiCamera() as camera:
+    camera.resolution=(1600,1200)
+    camera.start_preview()
+    time.sleep(1)
+    while(codebug.get_input('A') != 1):
+        time.sleep(.1)
+    filename=time.strftime("%H-%M--%d-%m-%Y")+".jpg"
+    print (filename)
+    camera.capture(filename)
 """
